b7c957efaed43595f3c548754f4c689769491051
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-06-04 13:53:21 +0500

    feat: implement server setup and goal management routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`✅ Server started on port ${port}`);
+});
